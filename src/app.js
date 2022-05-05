2bc8da90f4a4d43bc631b679d3e298823b3c1193
Rozpi <joserozpide@yahoo.es>
2022-05-05 16:28:27 +0000

    generador-de-dominio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombres = ["el", "nuestro"];
+  let adjetivos = ["genial", "estupendo"];
+  let sustantivo = ["corredor", "mapache"];
+  let dominio = [".com", ".es"];
+
+  let resultado = "";
+  for (let i in pronombres) {
+    for (let j in adjetivos) {
+      for (let k in sustantivo) {
+        for (let m in dominio) {
+          resultado = pronombres[i] + adjetivos[j] + sustantivo[k] + dominio[m];
+          console.log(resultado);
+        }
+      }
+    }
+  }
+  return resultado;
 };
